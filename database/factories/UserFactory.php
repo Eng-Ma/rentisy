4a025d0b1906a52732711ec37cc02a087f30c000
Hato20201 <92298349+Hato20201@users.noreply.github.com>
2026-07-18 22:46:58 +0300

    m

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,7 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),
+            'name' => "Admin User",
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
